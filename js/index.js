e3e03367c40ac3e1493aa721cc9195099114288b
ArthurQuintino <arthurquintino2304@gmail.com>
2025-07-22 11:28:26 -0300

    Carrosel finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-function left_carrosel() {}
+document.addEventListener("DOMContentLoaded", function () {
+  let view = document.querySelector(".carrosel__view");
+  margem = 0;
 
-function right_carousel() {}
+  document.getElementById("left").onclick = function () {
+    if (margem > 0) {
+      margem--;
+      view.style.marginLeft = `-${margem * 100}vw`;
+    }
+  };
+
+  document.getElementById("right").onclick = function () {
+    if (margem < 2) {
+      margem++;
+      view.style.marginLeft = `-${margem * 100}vw`;
+    }
+  };
+});
